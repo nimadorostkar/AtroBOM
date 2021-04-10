1d2f20cd88eeabcb6f70ed311a18bcfa9f6397fb
nima <54316106+nimadorostkar@users.noreply.github.com>
2021-04-10 12:36:05 +0430

    Update settings.py

--- bom/settings.py
+++ bom/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '68%jtx*w%(=gxshr&01gy@@*n!33ymhpuz+xf&lyk98$xlrge2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
